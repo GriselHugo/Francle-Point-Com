d5005b9b4fe48db3a2fee54afa0cdaeeb01eeaed
GriselHugo <hugo.grisel@epitech.eu>
2025-04-13 18:25:16 +0200

    [+] Fill cities database when server created

---
+++ server/src/database-utils.js
@@ -0,0 +1,50 @@
+const db = require('./database-config');
+
+const geoApiRoute = "https://geo.api.gouv.fr/communes?codeDepartement=06&fields=nom,code,codesPostaux,departement,region,population";
+
+const fillCitiesDB = () => {
+  const query = 'INSERT INTO cities (name, commune_code, postal_code, population, department_name, department_code, region_name, region_code, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())';
+
+  return fetch(geoApiRoute)
+    .then(response => response.json())
+    .then(data => {
+      const promises = data.map(city => {
+        return new Promise((resolve, reject) => {
+          db.query(query, [
+            city.nom,
+            city.code,
+            city.codesPostaux[0],
+            city.population,
+            city.departement.nom,
+            city.departement.code,
+            city.region.nom,
+            city.region.code
+          ], (error, results) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve();
+          });
+        });
+      });
+      return Promise.all(promises);
+    });
+};
+
+const checkAndFillCitiesDB = () => {
+  const checkQuery = 'SELECT COUNT(*) as count FROM cities';
+
+  return new Promise((resolve, reject) => {
+    db.query(checkQuery, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      if (results[0].count === 0) {
+        return fillCitiesDB().then(resolve).catch(reject);
+      }
+      resolve();
+    });
+  });
+};
+
+module.exports = { checkAndFillCitiesDB };
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
 //index.js
-
 const port = 4000;
 const app = require('./app-config');
+const { checkAndFillCitiesDB } = require('./database-utils');
 
-app.listen(4000, () => {
-  console.log(`server listening on port ${port}`);
-});
+checkAndFillCitiesDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error checking/filling cities database:', error);
+    process.exit(1); // Exit the process with an error code
+  });
