95290c00331f23ee73a67ec4e8c60b6a4882fada
GriselHugo <hugo.grisel@epitech.eu>
2025-04-13 18:05:54 +0200

    [+] Add db management requests

--- docker/mariadb/init.sql
+++ docker/mariadb/init.sql
@@ -8,13 +8,13 @@ USE `fpc-db`;
 CREATE TABLE IF NOT EXISTS `cities` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `commune_code` int(5) NOT NULL,
-  `postal_code` int(5) NOT NULL,
+  `commune_code` varchar(5) NOT NULL,
+  `postal_code` varchar(5) NOT NULL,
   `population` int(11) NOT NULL,
   `department_name` varchar(255) NOT NULL,
-  `department_code` int(3) NOT NULL,
+  `department_code` varchar(3) NOT NULL,
   `region_name` varchar(255) NOT NULL,
-  `region_code` int(3) NOT NULL,
+  `region_code` varchar(3) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
--- server/src/routes/manage-cities-db.js
+++ server/src/routes/manage-cities-db.js
@@ -4,9 +4,55 @@ const router = express.Router();
 const geoApiRoute = "https://geo.api.gouv.fr/communes?codeDepartement=06&fields=nom,code,codesPostaux,departement,region,population"
 
 /* Fill cities database */
+router.post('/fill-cities-db', (req, res) => {
+  const db = require('../database-config');
+
+  const query = 'INSERT INTO cities (name, commune_code, postal_code, population, department_name, department_code, region_name, region_code, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())';
+
+  fetch(geoApiRoute)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(city => {
+        db.query(query, [
+          city.nom,
+          city.code,
+          city.codesPostaux[0],
+          city.population,
+          city.departement.nom,
+          city.departement.code,
+          city.region.nom,
+          city.region.code
+        ], (error, results) => {
+          if (error) {
+            console.error('Database query error:', error);
+            res.status(500).send('Error filling cities database');
+            return;
+          }
+        });
+    });
+    res.status(200).send('Cities database filled successfully');
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+    res.status(500).send('Error filling cities database');
+  });
+});
 
 /* Delete all cities */
+router.delete('/delete-cities', (req, res) => {
+  const db = require('../database-config');
 
-/* Recreate cities database */
+  const query = 'DELETE FROM cities';
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Database query error:', error);
+      res.status(500).send('Error deleting cities');
+      return;
+    }
+
+    res.status(200).send('Cities deleted successfully');
+  });
+});
 
 module.exports = router;
